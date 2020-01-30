9a3843c111f94238a694a47b8e720cfe97c4cd59
segmarian <segmarian@gmail.com>
2020-01-30 10:37:24 +0100

    characterview additions more tables

--- character/views.py
+++ character/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib import messages
+from django.forms import ModelMultipleChoiceField
+from django.urls import reverse_lazy
 from django.views.generic import ListView, UpdateView
 
 from character.models import *
@@ -16,3 +19,44 @@ class CharacterUpdateView (UpdateView):
     model = Character
     template = "templates/character_detail.html"
     fields = '__all__'
+    success_url = reverse_lazy('character_detail')
+
+    def get_success_url(self):
+        messages.success(self.request, '%s updated' % self.request.POST["character"].name)
+        return reverse_lazy('character_detail', kwargs={'pk': self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        chosen_character = context['pk']
+        context['form'].fields['character_skill'] = \
+            ModelMultipleChoiceField(
+                CharacterSkill.objects.filter(character=chosen_character),
+                required=False
+            )
+        context['form'].fields['character_attributes'] = \
+            ModelMultipleChoiceField(
+                CharacterAttribute.objects.filter(character=chosen_character),
+                required=False
+            )
+        context['form'].fields['character_skills'] = \
+            ModelMultipleChoiceField(
+                CharacterSkill.objects.filter(character=chosen_character),
+                required=False
+            )
+        context['form'].fields['character_schticks'] = \
+            ModelMultipleChoiceField(
+                CharacterSchtick.objects.filter(character=chosen_character),
+                required=False
+            )
+        context['form'].fields['character_proficiencies'] = \
+            ModelMultipleChoiceField(
+                CharacterProficiency.objects.filter(character=chosen_character),
+                required=False
+            )
+        context['form'].fields['character_flaws'] = \
+            ModelMultipleChoiceField(
+                CharacterFlaw.objects.filter(character=chosen_character),
+                required=False
+            )
+
+        return context
